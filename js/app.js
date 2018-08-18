2b81fa40bf7ca5152fe387bce587a4e47b4e1d3b
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-08-18 14:20:20 -0700

    Add files via upload
    updated table and then will update front store page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
 var hoursOpen = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-
 var storeLocations = [];
 var footerTotals = [];
 
 var cookieForm = document.getElementById('cookie-form');
-var updateForm = document.getElementById('update-form');
 
 var patTable = document.getElementById('patTable');
 
@@ -39,24 +37,24 @@ Bakery.prototype.calcDailyCookieTotal = function(){
   this.dailyCookieTotal = x;
 };
 
-var pike = new Bakery('First and Pike', 23, 65, 6.3);
-var seaTac = new Bakery('SeaTac Airport', 3, 24, 3.7);
-var seattleCenter = new Bakery('Seattle Center', 11, 38, 2.3);
-var capitolHill = new Bakery('Capitol Hill', 20, 38, 2.3);
-var alki = new Bakery('Alki', 2, 16, 4.6);
+new Bakery('First and Pike', 23, 65, 6.3);
+new Bakery('SeaTac Airport', 3, 24, 3.7);
+new Bakery('Seattle Center', 11, 38, 2.3);
+new Bakery('Capitol Hill', 20, 38, 2.3);
+new Bakery('Alki', 2, 16, 4.6);
 
-pike.calcDailyCookieTotal();
-seaTac.calcDailyCookieTotal();
-seattleCenter.calcDailyCookieTotal();
-capitolHill.calcDailyCookieTotal();
-alki.calcDailyCookieTotal();
+// pike.calcDailyCookieTotal();
+// seaTac.calcDailyCookieTotal();
+// seattleCenter.calcDailyCookieTotal();
+// capitolHill.calcDailyCookieTotal();
+// alki.calcDailyCookieTotal();
 
-console.table(storeLocations);
-console.log(pike);
-console.log(seaTac);
-console.log(seattleCenter);
-console.log(capitolHill);
-console.log(alki);
+// console.table(storeLocations);
+// console.log(pike);
+// console.log(seaTac);
+// console.log(seattleCenter);
+// console.log(capitolHill);
+// console.log(alki);
 
 Bakery.prototype.render = function() {
   var trEl = document.createElement('tr');
@@ -97,6 +95,10 @@ function makeHeaderRow() {
 }
 // displays sales
 function displaySales() {
+  for (i = 0; i < storeLocations.length; i++) {
+    storeLocations[i].calcDailyCookieTotal();
+  }
+
   for(var i = 0; i < storeLocations.length; i++){
     storeLocations[i].render();
   }
@@ -108,7 +110,7 @@ function createFooter() {
   thEl.textContent = 'Hourly Totals';
   trEl.appendChild(thEl);
   patTable.appendChild(trEl);
-  console.log(createFooter);
+  // console.log(createFooter);
 
   for (var i = 0; i < hoursOpen.length; i++) {
     var tdEl = document.createElement('td');
@@ -134,7 +136,7 @@ for(var i = 0; i < hoursOpen.length; i++) {
   footerTotals[i] = 0;
   for(var j = 0; j < storeLocations.length; j++) {
     footerTotals[i] += storeLocations[j].cookiesPerHour[i];
-    console.log(footerTotals);
+    // console.log(footerTotals);
   }
 
 }
@@ -142,12 +144,16 @@ function newBakerySubmit (event) {
   event.preventDefault();
 
   var newName = event.target.newLocation.value;
-  var newMin = event.target.minimumCustomers.value;
-  var newMax = event.target.maximumCustomers.value;
-  var newAvg = event.target.avgCookiesPerCustomer.value;
+  var newMin = parseInt(event.target.minimumCustomers.value);
+  var newMax = parseInt(event.target.maximumCustomers.value);
+  var newAvg = parseInt(event.target.avgCookiesPerCustomer.value);
   new Bakery (newName, newMin, newMax, newAvg);
 
-  console.table(storeLocations);
+  // console.table(storeLocations);
+  // console.log('newMin', isNaN(newMin));
+  // console.log('newMax', isNaN(newMax));
+  // console.log('newAvg', newAvg);
+  // console.log(storeLocations[storeLocations.length-1]);
 
   // newBakery.render();
 
@@ -162,6 +168,9 @@ function newBakerySubmit (event) {
   makeHeaderRow();
   displaySales();
   createFooter();
+  console.log(storeLocations[storeLocations.length-1]);
+  console.log(storeLocations[storeLocations.length-2]);
+
 
 }
 
